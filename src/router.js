751205e6390b6fea3fb1b2165fedef86571c224e
Brandon Segovia <bsegovi9@gmail.com>
2025-04-20 20:12:14 -0700

    modified vite config and router.js

--- src/router.js
+++ src/router.js
@@ -6,11 +6,11 @@ import LoginPage from './views/LoginPage.vue'
 import SettingsPage from './views/SettingsPage.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/employees/:id', name: 'CardDetail', component: CardDetail },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage },
+  { path: '/homework5-api/', name: 'Home', component: HomePage },
+  { path: '/homework5-api/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/homework5-api/employees/:id', name: 'CardDetail', component: CardDetail },
+  { path: '/homework5-api/login', name: 'LoginPage', component: LoginPage },
+  { path: '/homework5-api/settings', name: 'SettingsPage', component: SettingsPage },
 ]
 
 const router = createRouter({
